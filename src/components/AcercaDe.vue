ce71af2d0a5b82e0e99c33c70a67b8cbce255348
@joseavera <69088516+joseavera@users.noreply.github.com>
2024-03-23 20:26:26 -0600

    cambio acerca de

--- src/components/AcercaDe.vue
+++ src/components/AcercaDe.vue
@@ -1,9 +1,9 @@
 <template>
   <v-card class="py-6 px-6" flat>
     <v-alert type="info">
-      <h2>Acerca de</h2>
+      <h2>Acerca de: </h2>
       <p>Creado y mantenido por: <a class="text-button" href="https://parzibyte.me/blog">Parzibyte</a>.
-        Código fuente disponible en <a href="https://parzibyte.me/blog">parzibyte.me/blog</a>
+        Código fuente disponible en: <a href="https://parzibyte.me/blog">parzibyte.me/blog</a>
       </p>
       <h2>Créditos</h2>
       <p> Iconos diseñados por: <a href="https://www.freepik.com" title="Freepik">Freepik</a> from <a
